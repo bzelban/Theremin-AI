c9d5b0d31c803b3ca108607b021e686c22276565
B!_NULL <bzelban@outlook.com>
2020-12-29 12:24:51 +0300

    resizer is done

--- Dataset/_kitchen/resizer.py
+++ Dataset/_kitchen/resizer.py
@@ -1,24 +1,32 @@
-##############################################################
-# TO USE THIS SCRIPT
-#
-#
-#
-#
-##############################################################
+
+#           TO USE THIS SCRIPT
+"""
+    1- run "pip3 install Pillow"
+    2- Copy raw images to _raw folder
+    3- Run script only
+    4- Take resized images from _resized folder
+    
+    This Script will resize the photos automatically
+"""
 
 from PIL import Image
 import os
 import argparse
 
-def rescale_images(directory, size):
-    from img in os.listdir(directory):
+def rescale_images(directory, out, size):
+    
+    dirs = os.listdir(directory)
+    
+    for img in dirs:
         im = Image.open(directory + img)
         im_resizer = im.resize(size, Image.ANTIALIAS)
-        im_resizer.save(directory + img)
+        im_resizer.save(out + img)
+        
+    print('Image Resizing Process is Done')
+    
 
 if __name__ == '__main__':
+    
     parser = argparse.ArgumentParser(description="Rescale Images")
-    parser.add_argument('-d', '--directory', type=str, required=True, help='Directory that contains the images')
-    parser.add_argument('-s', '--size', type=int, nargs=2, metavar=('width', 'height'), help='Image Size to transform')
     args = parser.parse_args()
-    rescale_images(args.directory, args.size)
+    rescale_images('_raw/', '_resized/', (800, 600))
